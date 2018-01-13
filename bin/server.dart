6eccb37a961480699ec25e99deee428da0f902d6
10154507101 <1401702296@qq.com>
2018-01-13 11:24:07 +0800

    组合2

--- bin/server.dart
+++ bin/server.dart
@@ -6,16 +6,46 @@ import 'package:sqljocky5/sqljocky.dart';
 import 'package:sqljocky5/utils.dart';
 import 'dart:async';
 import 'dart:convert';
+//import 'dart:html';
+String data;
 
 @app.Route("/data/")
 helloWorld() {
   return getDataFromDB();
 }
 
+@app.Route("/day1/")
+helloIelts() {
+  return getIeltsFromDB();
+}
+
 @app.Route("/data/add", methods: const [app.POST])
 addUser(@app.Body(app.TEXT) String userData) {
-  String data = userData;
+   data = userData;
   return data;
+}
+
+@app.Route("/data/addword", methods: const [app.POST])
+addword(@app.Body(app.TEXT) String userData) {
+   data = userData;
+  return getWordFromDB(data);
+}
+
+Future<String> getWordFromDB(String data) async {
+  var pool = new ConnectionPool(
+      host: 'www.muedu.org',
+      port: 3306,
+      user: 'deit-2015',
+      password: 'deit@2015!',
+      db: 'project_2015_1',
+      max: 5);
+  var results = await pool.query("select words.word, means.means from words,means where word='" + data + "' and words.ID=means.wordID ");
+  //todo get data from db.
+  String response;
+  await results.forEach((row) { 
+     response =''' ${row[0]}   ${row[1]}''';
+  }); 
+  return response;
 }
 
 Future<String> getDataFromDB() async {
@@ -24,18 +54,51 @@ Future<String> getDataFromDB() async {
       port: 3306,
       user: 'deit-2015',
       password: 'deit@2015!',
-      db: 'project_2015_example',
+      db: 'project_2015_1',
       max: 5);
-  var results = await pool.query('select iduser, firstname,lastname from user');
-  //todo get data from db.
+  var results = await pool.query("select words.word, means.means from words,means where word='" + data + "' and words.ID=means.wordID ");
   String response;
   await results.forEach((row) { 
-     response =JSON.encode([ "FirstName: ${row[1]}"," LastName: ${row[2]}"]);
- // response =JSON.encode(["1", "2", "bar"]);
-  });
-  return response;
 
-  // return '''["name1","name2","name33","name43"]''';
+     response =JSON.encode([" ${row[0]}"," ${row[1]}"]);
+    //  response=JSON.encode("\u8d44\u6599\uff0c\u6750\u6599");
+  }); 
+  return response;
+}
+
+Future<String> getIeltsFromDB() async {
+  var pool = new ConnectionPool(
+      host: 'www.muedu.org',
+      port: 3306,
+      user: 'deit-2015',
+      password: 'deit@2015!',
+      db: 'project_2015_1',
+      max: 5);
+
+    String response;
+    var WLIST = new Map();
+    
+    int i=1;
+    //var results = await pool.query("select Ielts.word, Ielts.means from Ielts where id='  "+ j +"  ' ");
+    var results = await pool.query("select Ielts.word, Ielts.means from Ielts where id<41");
+       
+     await results.forEach((row) { 
+ 
+    // response = JSON.encode([response,"${row[0]} , ${row[1]}"]);
+    response='''${row[0]} , ${row[1]}''';
+    WLIST[i]=response;
+    i++;
+    
+    // response=JSON.encode("\u8d44\u6599\uff0c\u6750\u6599");
+    
+    
+   }); 
+    response = JSON.encode([WLIST[1],WLIST[2],WLIST[3],WLIST[4],WLIST[5],WLIST[6],WLIST[7],WLIST[8],WLIST[9],WLIST[10],
+                           WLIST[11],WLIST[12],WLIST[13],WLIST[14],WLIST[15],WLIST[16],WLIST[17],WLIST[18],WLIST[19],WLIST[20],
+                           WLIST[21],WLIST[22],WLIST[23],WLIST[24],WLIST[25],WLIST[26],WLIST[27],WLIST[28],WLIST[29],WLIST[30],
+                           WLIST[31],WLIST[32],WLIST[33],WLIST[34],WLIST[35],WLIST[36],WLIST[37],WLIST[38],WLIST[39],WLIST[40]]);
+    print(response); 
+    return response;
 }
 
 @app.Route("/register/")
--- web/index.dart
+++ web/index.dart
@@ -10,16 +10,109 @@ import 'dart:html';
 
 import 'package:http/browser_client.dart';
 InputElement toDoInput;
-
-main() async {
-  querySelector('#getData').onClick.listen(makeRequest);
-
-  querySelector('#search_word').onClick.listen(makePostRequest);
-
-  wordList = querySelector('#wordList');
-}
+UListElement toDoList;
+UListElement DoList;
+ButtonElement deleteAll;
+ButtonElement addnewword;
+UListElement firstlist;
+UListElement secondlist;
+UListElement thirdlist;
+UListElement fourthlist;
+UListElement l1;
+ButtonElement A;
 
 var wordList;
+var firstpage=document.getElementById("firstpage");
+String responseText;
+var newword=new Map();
+int countNewWord=0;
+String b;
+
+main() async {
+  
+
+  querySelector('#search_word').onClick.listen(makePostRequest);
+  querySelector('#search_word').onClick.listen(makeRequest);
+  wordList = querySelector('#wordList');
+  firstlist = querySelector('#firstlist');
+  secondlist = querySelector('#secondlist');
+  thirdlist = querySelector('#thirdlist');
+  fourthlist = querySelector('#fourthlist');
+  toDoList = querySelector('#sample_list_id');  
+ toDoList.onChange.listen(addToDoItem);
+ // toDoList.onClick.listen(removeItem);
+ addnewword= querySelector('#add-newword'); 
+ addnewword.onClick.listen(addToDoItem);
+ deleteAll= querySelector('#delete-all'); 
+ deleteAll.onClick.listen(deleteAllElements);
+ A=querySelector('#Index');
+ A.onClick.listen(CHANGE);
+ querySelector('#Index').onClick.listen(CHANGE);
+
+
+  var path = 'http://localhost:90/day1/';
+   listString(await HttpRequest.getString(path));
+  
+}
+
+
+void CHANGE(Event e){
+ l1=querySelector('#a');
+ l1.text='qqq';
+ 
+  firstpage.style.display='none';
+
+}
+void addToDoItem(Event e){ 
+  int i;
+  var nextToDo = new LIElement();
+  if(countNewWord==0)
+  {
+  newword[countNewWord++]=responseText;
+  nextToDo.text=  responseText;
+  toDoList.children.add(nextToDo); 
+    }
+  else 
+  {
+    for( i=0;i<countNewWord;i++)
+    {
+      if(newword[i]==responseText){
+      querySelector('#attention').text="您已添加该词";
+      break;
+      }
+    }
+    if (i==countNewWord)
+    {
+       newword[countNewWord++]=responseText;      
+       nextToDo.text=  responseText;
+       toDoList.children.add(nextToDo);        
+    }
+  }
+  nextToDo.onClick.listen((e)=>nextToDo.remove());
+  nextToDo.onClick.listen((e)=>b=nextToDo.text);
+  nextToDo.onClick.listen(deletedata);
+ 
+}
+
+void deletedata(Event e)
+{
+  for(int i=0;i<countNewWord;i++)
+  {
+    if(b==newword[i])
+    {
+      for(int j=i;j<countNewWord;j++)newword[j]=newword[j+1];
+   // newword[i]="1";
+      break;
+    }
+  }
+ // newword[0]="1";
+// querySelector('#attention').text="您已删除";
+}
+
+void deleteAllElements(Event e) {
+  toDoList.children.clear();
+}
+
 
 void handleError(Object error) {
   wordList.children.add(new LIElement()..text = 'Request failed.');
@@ -38,27 +131,27 @@ Future makeRequest(Event e) async {
 void processString(String jsonString) {
   List<String> portmanteaux = JSON.decode(jsonString ) as List<String>;
   //List<String> a=JSON.getData()
+  wordList.children.clear();
   for (int i = 0; i < portmanteaux.length; i++) {
     wordList.children.add(new LIElement()..text = portmanteaux[i]);
   }
 }
 
-Future makeSearchRequest(Event e) async {
-  var path = 'http://localhost:90/data/';
-  try {
-    processSearchString(await HttpRequest.getString(path));
-  } catch (e) {
-    print('Couldn\'t open $path');
-    handleError(e);
+void listString(String jsonString) {
+   List<String> portmanteaux = JSON.decode(jsonString ) as List<String>;
+     for (int i = 0; i < 10; i++) {
+    firstlist.children.add(new LIElement()..text = portmanteaux[i]);
   }
-}
-
-void processSearchString(String jsonString) {
-  List<String> portmanteaux = JSON.decode(jsonString ) as List<String>;
-  //List<String> a=JSON.getData()
-  for (int i = 0; i < portmanteaux.length; i++) {
-    wordList.children.add(new LIElement()..text = portmanteaux[i]);
+       for (int i = 10; i < 20; i++) {
+   secondlist.children.add(new LIElement()..text = portmanteaux[i]);
   }
+    for (int i = 20; i < 30; i++) {
+    thirdlist.children.add(new LIElement()..text = portmanteaux[i]);
+  }
+    for (int i = 30; i < portmanteaux.length; i++) {
+    fourthlist.children.add(new LIElement()..text = portmanteaux[i]);
+  }
+  
 }
 
 
@@ -66,11 +159,12 @@ Future makePostRequest(Event e) async {
   
   toDoInput=querySelector('#search');
   String element=toDoInput.value;
-  String url = 'http://localhost:90/data/add';
+  var url = 'http://localhost:90/data/addword';
   HttpRequest
       .request(url, method: 'POST', sendData:element )
       .then((HttpRequest resp) {
     // Do something with the response.
-    querySelector('#response').text = resp.responseText;
+     responseText = resp.responseText;
   });
 }
+
